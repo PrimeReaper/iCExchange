59859680e4b46cb80dffb9c79528a2cb9597e141
Dmitriy Katkov <primereaper@Dmitriys-MacBook-Pro.local>
2015-01-24 21:47:36 +0300

    Start

---
+++ iCExchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCExchange
+//
+//  Created by Dmitriy Katkov on 24/01/15.
+//  Copyright (c) 2015 KKPR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iCExchangeTests/iCExchangeTests.m
@@ -0,0 +1,40 @@
+//
+//  iCExchangeTests.m
+//  iCExchangeTests
+//
+//  Created by Dmitriy Katkov on 24/01/15.
+//  Copyright (c) 2015 KKPR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iCExchangeTests : XCTestCase
+
+@end
+
+@implementation iCExchangeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
